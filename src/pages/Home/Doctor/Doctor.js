7257c43bf37ef72223b8e2d49e0bb04bb30fb749
FazleRabbiRana <ranafazrabbi@gmail.com>
2021-10-20 21:04:06 +0600

    doctor card hover updated

--- src/pages/Home/Doctor/Doctor.js
+++ src/pages/Home/Doctor/Doctor.js
@@ -9,9 +9,9 @@ const Doctor = ({ doctor }) => {
 			<div className="overflow-hidden">
 				<img src={image} alt={name} />
 			</div>
-			<div className="px-2 lg:px-4 pb-6 absolute bottom-0 inset-x-0 h-1/2 flex flex-col justify-end text-white bg-gradient-to-t from-my-primary-dark via-my-primary-light">
+			<div className="px-2 lg:px-4 pb-4 absolute bottom-0 inset-x-0 h-1/2 flex flex-col justify-end text-white bg-gradient-to-t from-my-primary-dark via-my-primary-light">
 				<h3 className="text-xl md:text-sm lg:text-xl xl:text-2xl text-white mb-2">{name}</h3>
-				<p className="text-semibold text-sm md:text-xs lg:text-sm font-my-serif">{description}</p>
+				<p className="text-semibold text-sm md:text-xs lg:text-sm font-my-serif overflow-hidden max-h-0 duration-300 group-hover:max-h-full">{description}</p>
 			</div>
 		</div>
 	);
